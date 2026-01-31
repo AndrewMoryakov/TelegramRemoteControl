e8bdf656ead90ed3a888f854b865d7e013296eac
Andrew Moryakov <topazd2@gmail.com>
2026-01-31 19:04:57 +0300

    chore: remove legacy monolith

--- src/BotSettings.cs
+++
@@ -1,7 +0,0 @@
-namespace TelegramRemoteControl;
-
-public class BotSettings
-{
-    public string Token { get; set; } = string.Empty;
-    public long[] AuthorizedUsers { get; set; } = Array.Empty<long>();
-}
